3a70221fdb29ca12b6320a7bcac3d3afa42bb923
Mike Pennisi <mike@mikepennisi.com>
2013-11-27 15:48:50 -0500

    Implement DOM Level 1 `Node` API

--- index.js
+++ index.js
@@ -58,6 +58,10 @@ DomHandler.prototype._addDomElement = function(element){
 
 	element.next = null;
 
+	if (this._options.withDomLvl1) {
+		element.__proto__ = NodePrototype;
+	}
+
 	if(previousSibling){
 		element.prev = previousSibling;
 		previousSibling.next = element;
@@ -68,6 +72,48 @@ DomHandler.prototype._addDomElement = function(element){
 	siblings.push(element);
 	element.parent = parent || null;
 };
+
+// This object will be used as the prototype for Nodes when creating a
+// DOM-Level-1-compliant structure.
+var NodePrototype = {
+	get firstChild() {
+		var children = this.children;
+		return children && children[0] || null;
+	},
+	get lastChild() {
+		var children = this.children;
+		return children && children[children.length - 1] || null;
+	},
+	get nodeType() {
+		return nodeTypes[this.type] || nodeTypes.element;
+	}
+};
+var domLvl1 = {
+	tagName: 'name',
+	childNodes: 'children',
+	parentNode: 'parent',
+	previousSibling: 'prev',
+	nextSibling: 'next',
+	nodeValue: 'data'
+};
+var nodeTypes = {
+	element: 1,
+	text: 3,
+	cdata: 4,
+	comment: 8
+};
+Object.keys(domLvl1).forEach(function(key) {
+	var shorthand = domLvl1[key];
+	Object.defineProperty(NodePrototype, key, {
+		get: function() {
+			return this[shorthand] || null;
+		},
+		set: function(val) {
+			this[shorthand] = val;
+			return val;
+		}
+	});
+});
 
 DomHandler.prototype.onopentag = function(name, attribs){
 	var lastTag = this._tagStack[this._tagStack.length - 1];
